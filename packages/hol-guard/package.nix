8a3f6c77761935a284e49300e55b11f34ce9becb
Michael Kantor <6068672+kantorcodes@users.noreply.github.com>
2026-09-18 02:22:18 -0400

    hol-guard: init at 3.0.189

---
+++ packages/hol-guard/package.nix
@@ -0,0 +1,79 @@
+{
+  lib,
+  python3,
+  fetchurl,
+  versionCheckHook,
+  versionCheckHomeHook,
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "hol-guard";
+  version = "3.0.189";
+  pyproject = true;
+
+  src = fetchurl {
+    url = "https://pypi.org/packages/source/h/hol-guard/hol_guard-${version}.tar.gz";
+    hash = "sha256-XlKxbWpA6eAfVMENfg6M+pdsLmxT0U13RPh9u+MF8Iw=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'hatchling<1.31' 'hatchling'
+  '';
+
+  build-system = with python3.pkgs; [
+    hatchling
+  ];
+
+  dependencies = with python3.pkgs; [
+    cryptography
+    idna
+    jsonschema
+    keyring
+    mcp
+    packaging
+    publicsuffixlist
+    pyyaml
+    regex
+    requests
+    rich
+  ];
+
+  pythonRelaxDeps = [
+    "idna"
+    "publicsuffixlist"
+    "rich"
+  ];
+
+  pythonImportsCheck = [ "codex_plugin_scanner" ];
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [
+    versionCheckHook
+    versionCheckHomeHook
+  ];
+  versionCheckProgramArg = "--version";
+
+  passthru.category = "Utilities";
+
+  meta = {
+    description = "Open-source antivirus and runtime protection for AI agents";
+    homepage = "https://hol.org/guard";
+    changelog = "https://github.com/hashgraph-online/hol-guard/releases";
+    license = lib.licenses.asl20;
+    sourceProvenance = with lib.sourceTypes; [ fromSource ];
+    maintainers = [
+      {
+        github = "kantorcodes";
+        githubId = 6068672;
+        name = "Michael Kantor";
+      }
+    ];
+    mainProgram = "hol-guard";
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+  };
+}
